086b5837201fec32d8de76bd1e71f5531a7d77f3
karlcc1 <65652092+karlcc1@users.noreply.github.com>
2020-06-09 19:18:44 +1200

    genesis1
    -Revised menu
    -Added menu features (add, remove)

--- a.py
+++ a.py
@@ -25,6 +25,28 @@ games.append(game2)
 games.append(game3)
 games.append(game4)
 
+#Main Menu
+
+def select_menu():
+    print("=="*32)
+    print("*Game Database")
+    print()
+    print("A. Show Library")
+    print("B. Add Game")
+    print("C. Delete Game")
+    print("D. Edit Game")
+    print("Z. Exit")
+    print()
+    menu_letter = input("Input:")
+    if menu_letter == "A":
+        show_games()
+    elif menu_letter == "B":
+        add_game()
+    elif menu_letter == "C":
+        del_game()
+    elif menu_letter == "Z":
+        exit_program()
+
 def show_games():
     print()
     print("=="*32)
@@ -32,14 +54,34 @@ def show_games():
     print()
     for g in games:
         print(g)
+    select_menu()
 
-print("Game Database")
-print()
-print("A. Show Library")
-print("Z. Exit")
-print()
-print("Input:")
-menu_select = input()
+#incomplete
+def add_game():
+    print("=="*32)
+    print("Add new game . . .")
+    print()
+    print("Please type game info in following format [Title, Genre, Year, Developer, Publisher, Completed (boolean)]:")
+    new_game = input()
+    #
+    #print("New game added!")
+    select_menu()
 
-if menu_select == "A":
-    show_games()
+def del_game():
+    print("=="*32)
+    print("Delete game . . .")
+    print()
+    game_to_del = input("Please type game title:")
+    print()
+    for g in games:
+        if g.title == game_to_del:
+            games.remove(g)
+    print(game_to_del + " has been deleted!")
+    select_menu()
+
+def exit_program():
+    print("Goodbye!")
+
+select_menu()
+
+
